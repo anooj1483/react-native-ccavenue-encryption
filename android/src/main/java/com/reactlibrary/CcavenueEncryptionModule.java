3f4a0b2ec4f2a661afbb1ce5b4368e66317bbf8c
Anooj Krishnan G <anooj1483@gmail.com>
2020-01-19 22:54:41 +0530

    [Feature] - Encryption Moduke

--- android/src/main/java/com/reactlibrary/CcavenueEncryptionModule.java
+++ android/src/main/java/com/reactlibrary/CcavenueEncryptionModule.java
@@ -1,9 +1,21 @@
 package com.reactlibrary;
 
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
 
 public class CcavenueEncryptionModule extends ReactContextBaseJavaModule {
 
@@ -24,4 +36,29 @@ public class CcavenueEncryptionModule extends ReactContextBaseJavaModule {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
+
+    @ReactMethod
+    public void encrypt(String plainText, String key, Promise promise){
+        try{
+            Log.e("encrypting",""+plainText);
+            Log.e("encrypting_Key",""+key);
+            PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.decode(key, Base64.DEFAULT)));
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            String encVal = Base64.encodeToString(cipher.doFinal(plainText.getBytes("UTF-8")),Base64.DEFAULT);
+            WritableMap params = Arguments.createMap();
+            params.putString("encrypted", encVal);
+            promise.resolve(params);
+        }catch (Exception e) {
+            e.printStackTrace();
+            promise.reject("Error", e);
+        }
+
+    }
+
+//    @ReactMethod
+//    public void getBytesFromString(String val){
+//        WritableMap params = Arguments.createMap();
+//        //params.put("bytes",val.getBytes());
+//    }
 }
